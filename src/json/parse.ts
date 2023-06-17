16ca95d8a516b6b5d86b28590225d6ffa069618d
Riri <Daydreamerriri@outlook.com>
2023-06-18 00:56:47 +0800

    feat: speed up parse

--- src/json/parse.ts
+++ src/json/parse.ts
@@ -4,36 +4,42 @@ const DEV_DEPS = 'devDependencies'
 const DEPS = 'dependencies'
 export const RE_VERSION = /^[ \t]*"(\S+?)"([ \t]*:[ \t]*)(")(.*?)\3/
 
+const depFieldsRe = /"(devD|d)ependencies"[ \t]*:[ \t]*{[\s\S]*?}/g
+
 export function parseJson(text: string) {
   const pkg = JSON.parse(text)
   const deps = { ...(pkg[DEPS] ?? {}), ...(pkg[DEV_DEPS] ?? {}) }
   const depNames = Object.keys(deps)
 
-  const depsPos = text.indexOf(`"${DEPS}"`)
-  const devDepsPos = text.indexOf(`"${DEV_DEPS}"`)
+  const depfieldsMatchs = text.matchAll(depFieldsRe)
+  const ranges = [...depfieldsMatchs].map((match) => {
+    const start = match.index ?? 0
+    const end = start + match[0].length
+    return { start, end }
+  })
 
-  const depItems = depNames.map((name) => {
-    const reg = new RegExp(`"${name}".*,?`, 'g')
-    const minPos = name in (pkg[DEPS] ?? {}) ? depsPos : devDepsPos
-    let match: RegExpExecArray | null
-    while (match = reg.exec(text)) {
-      if (match.index > minPos)
-        break
-    }
+  const reg = new RegExp(`"(${depNames.join('|')})"([ \\t]*:[ \\t]*)(")(.*?)\\3`, 'g')
 
-    if (!match)
-      throw new Error('Can\'t find deps')
-
-    const startInline = match[0].indexOf('"', name.length + 2)
-    const endInline = match[0].indexOf('"', startInline + 1)
-
+  const matchs = text.matchAll(reg)
+  const validMatchs = [...matchs].filter((match) => {
+    const { index } = match
+    return index !== undefined
+      && ranges.some(
+        range => index > range.start && index < range.end,
+      )
+  })
+  const items = validMatchs.map((match) => {
+    const [origin, key, mid, _, value] = match
+    const start = (match.index ?? 0) + key.length + mid.length + 2
+    const end = (match.index ?? 0) + origin.length
     return new Item({
-      key: name,
-      value: deps[name],
-      start: match.index + startInline,
-      end: match.index + endInline,
+      key,
+      value,
+      start,
+      end,
       values: [],
     })
   })
-  return depItems
+
+  return items
 }
