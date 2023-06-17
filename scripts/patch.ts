52d7374a0f21c78832082bde1d13beef35e4fef3
Riri <Daydreamerriri@outlook.com>
2023-06-18 04:04:34 +0800

    chore: remove file

--- scripts/patch.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs'
-import path from 'node:path'
-
-const origin = 'require.resolve("node-gyp/bin/node-gyp.js")'
-const patched = '""'
-const targetPath = path.resolve(__dirname, '../dist/index.js')
-const content = fs.readFileSync(targetPath, 'utf-8')
-fs.writeFileSync(targetPath, content.replace(origin, patched))
