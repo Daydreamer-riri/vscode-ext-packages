b4bce7350d976b3eed449919c06b738b91d9048e
Riri <Daydreamerriri@outlook.com>
2023-06-30 16:43:51 +0800

    refactor: switch from npm view to npm-registry-fetch

--- src/api/version.ts
+++ src/api/version.ts
@@ -1,10 +1,46 @@
+import fetch from 'npm-registry-fetch'
 import { execCmd } from '../utils/cmd'
 
+const NPM_REGISTRY = 'https://registry.npmjs.org/'
+
+const mockPacoteVersion = '15.2.0'
+
+function Header(name: string) {
+  return {
+    'user-agent': `pacote/${mockPacoteVersion} node/${process.version}`,
+    'pacote-version': mockPacoteVersion,
+    'pacote-req-type': 'packument',
+    'pacote-pkg-id': `registry:${name}`,
+    'accept': 'application/json',
+  }
+}
+
 export async function version(name: string, cwd: string) {
-  const cmd = `npm view ${name} versions`
-  const versions = await execCmd(cmd, cwd)
-  if (!versions)
-    return []
-  const matchs = versions.matchAll(/'(.*?)'/g)
-  return [...matchs].map(match => match[1])
+  const registry = await getNpmRegistry(name, cwd)
+  try {
+    const data = await fetch.json(name, { registry, fullMetadata: true, headers: Header(name) })
+    const versions = Object.keys(data.versions || {})
+    return versions
+  }
+  catch {
+    return null
+  }
+}
+
+const registryCache = new Map<string, string>()
+
+export async function getNpmRegistry(pkg: string, cwd: string) {
+  const key = `${pkg}+++${cwd}`
+  if (registryCache.has(key))
+    return registryCache.get(key)!
+
+  const cmd = 'npm config get registry'
+  const scopedCmd = `npm config get ${pkg}:registry`
+  const [defaultRegistry, scopedRegistry] = await Promise.all([
+    execCmd(cmd, cwd).catch(() => null),
+    execCmd(scopedCmd, cwd).catch(() => null),
+  ])
+
+  registryCache.set(key, scopedRegistry || defaultRegistry || NPM_REGISTRY)
+  return scopedRegistry || defaultRegistry || NPM_REGISTRY
 }
